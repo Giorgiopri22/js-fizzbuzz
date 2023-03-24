a61f93ff4fc1a302260d01107b09f33c0f6e1222
Giorgiopri22 <giorgioprizzi@gmail.com>
2023-03-24 16:27:03 +0100

    FIzzBuzz

--- main.js
+++ main.js
@@ -4,18 +4,24 @@ const gridContainer = document.getElementById("grid-container");
 // Loop attraverso i numeri da 1 a 100
 for (let i = 1; i <= 100; i++) {
   let result = "";
-  
-  
-  if (i % 3 === 0) {
-    result += "Fizz";
-  }
-  if (i %  5=== 0) {
-    result += "Buzz";
-  }
-  
   const gridItem = document.createElement("div");
   gridItem.classList.add("grid-item");
-  gridItem.textContent = result || i;
-
   gridContainer.appendChild(gridItem);
+    
+  if (i % 3 === 0) {
+    result += "Fizz";
+   // gridItem.classList.add("Fizz");
+  }
+  if (i %  5=== 0) {  
+    result += "Buzz";
+    //gridItem.classList.add("Buzz");
+  }
+  if(result=="FizzBuzz"){
+    //gridItem.classList.add("FizzBuzz")
+  }
+  if(result!=""){
+      gridItem.classList.add(result);
+  }
+  
+  gridItem.textContent = result || i ;
 }
